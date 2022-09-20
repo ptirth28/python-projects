43a660a4b244afbc5b9eb872e3efeae110a1d864
Tirth Patel <tirthpatel28898@gmail.com>
2022-09-20 00:32:07 -0400

    famous person added in madlibs code

--- madlibs.py
+++ madlibs.py
@@ -9,6 +9,7 @@
 adj = input("Adjective: ")
 verb1 = input("Verb 1: ")
 verb2 = input("verb 2: ")
+famous_person = input("Famous Person: ")
 
 print(f"Computer programming is so {adj}! It makes me so excited all the time becaue I love to {verb1}. \
     Stay hydrated and {verb2} like you are {famous_person}!")
