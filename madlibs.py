c5f9ca4a2f79294b5e277d3912523eada1d371fc
Tirth Patel <tirthpatel28898@gmail.com>
2022-09-20 00:20:47 -0400

    my first commit of mini project

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# # string concatination (aka how to put string together)
+# # suppose we want to create a string that says "suscribe to ______"
+# youtuber = "tirth patel"
+
+# print("suscriber to "+ youtuber)
+# print("suscribe to {} please.".format(youtuber))
+# print(f"suscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("verb 2: ")
+
+print(f"Computer programming is so {adj}! It makes me so excited all the time becaue I love to {verb1}. \
+    Stay hydrated and {verb2} like you are {famous_person}!")
