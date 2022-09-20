a4754ded73f4a18e06639f9466f802ea98262281
Tirth Patel <tirthpatel28898@gmail.com>
2022-09-20 13:42:24 -0400

    guess the number user

---
+++ guess_the_number_user.py
@@ -0,0 +1,17 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f"Yay, The computer guessed your number, {guess}, correctly!!")
+
+computer_guess(8)
